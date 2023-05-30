59a83407d80456d85f4b6b8cc00e6c617821dc4e
Jonathas Kennedy ALves Pereira <jonathaslan@hotmail.com>
2023-05-30 19:57:52 -0300

    adicionado teste

---
+++ tests.py
@@ -0,0 +1,15 @@
+from main import *
+
+
+nome_arquivo, dados_txt = ler_arquivo("teste.txt")
+tipo_calculo = escolhe_calculo(1)
+tem_solo = escolhe_solo(1)
+
+# depuração de leitura do arquivo toml formatado e imprime na tela
+new_toml_string = toml.dumps(dados_txt)
+print(new_toml_string)
+
+nome_arquivo, NomePastaResultados = gerencia_pastas(
+    nome_arquivo, tipo_calculo)
+
+grava_geo(nome_arquivo, dados_txt, tem_solo)
